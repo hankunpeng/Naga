028b271de116b76d9678e680a61a9b9d4095d244
Kunpeng Han <hankunpeng@gmail.com>
2017-09-25 12:19:46 +0800

    adds Python script files

---
+++ ayo.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+print('Please enter your name: ')
+name = input()
+print('Are you OK,', name + '?')
---
+++ hi.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+print('Hi, Emma!')
+print('Hi,', 'Emma!')
